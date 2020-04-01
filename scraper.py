90d380d765a3756b5121a09a55629961562e6dc1
Michał <michalr101@gmail.com>
2020-04-01 12:56:02 +0200

    18.03

--- scraper.py
+++ scraper.py
@@ -13,17 +13,39 @@ page_tree = BeautifulSoup(page_respons.text, 'html.parser')
 opinions = page_tree.find_all("li", "review-box")
 
 #wydobycie składowych dla pojedynczyej opinii
-opinion = opinions.pop()
+for opinion in opinions:
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find("div", "reviewer-name-line").string
+    try:
+        recomendation = opinion.find("div", "product-review-summary").find("em").string
+    except AttributeError:
+        recomendation = None
 
-opinion_id = opinion["data-entry-id"]
-author = opinion.find("div", "reviewer-name-line").string
-recomendation = opinion.find("div", "product-review-summary").find("em").string
-stars = opinion.find("span", "review-score-count").string
-pucharsed = opinion.find("div", "product-review-pz").string
-# - data wystawienia: span.review-time > time["datetime"] - pierwszy element listy
-# - data zakupu: span.review-time > time["datetime"] - drugi element listy
-useful = opinion.find("button", "vote-yes").find("span").string
-useless = opinion.find("button", "vote-no").find("span").string
-content = opinion.find("p", "product-review-body").get_text()
-# - wady: div.cons-cell > ul
-# - zalety: div.pros-cell > ul
+    stars = opinion.find("span", "review-score-count")
+    try:
+        purchased = opinion.find("div", "product-review-pz")
+    except AttributeError:
+        purchased = None
+    dates = opinion.find("span", "review-time").find_all("time")
+    review_date = dates.pop(0)["datetime"]
+    try:
+        purchase_date = dates.pop(0)["datetime"]
+    except AttributeError:
+        purchase_date = None
+
+    useful = opinion.find("button", "vote-yes").find("span").string
+    useless = opinion.find("button", "vote-no").find("span").string
+    content = opinion.find("p", "product-review-body").get_text()
+
+    try:
+        pros = opinion.find("div", "pros-cell").find("ul").get_text()
+    except AttributeError:
+        pros = None
+
+    try:
+        cons = opinion.find("div", "cons-cell").find("ul").get_text()
+    except AttributeError:
+        cons = None
+
+    #print(opinion_id, author, recomendation, useful, useless, content, stars, purchased)
+    print(opinion_id, recomendation, stars, content, author, pros, cons, useful, useless, purchased, purchase_date, review_date)
