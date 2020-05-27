eedfd328697cc893c0d2cc8b0a8633454e99d156
Michał <michalr101@gmail.com>
2020-05-27 15:02:05 +0200

    27-05

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from flaskext.markdown import Markdown
 from app.forms import ProductForm
 from app.models import Opinion, Product
 import requests
+import pandas as pd
 Markdown(app)
 
 app.config['SECRET_KEY'] = "TajemniczyMysiSprzęt"
@@ -37,6 +38,21 @@ def extract():
 
 @app.route('/product/<id>')
 def product(id):
+    product = Product(id)
+    product.read_product()
+    opinions = pd.DataFrame.from_records([opinion.__dict__() for opinion in product.opinions])
+    opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+    return render_template(
+        "product.html",
+        tables=[
+            opinions.to_html(
+                classes='table table-bordered table-sm table-responsive',
+                table_id = "opinions",
+                index = False
+            )
+        ], 
+        titles=opinions.columns.values 
+    )
     pass
 
 @app.route('/products')
